9cc821a85a458bc65388afbe7c099c21a12d022c
KluasMM <matian0720@foxmail.com>
2022-07-21 23:27:32 +0800

    [feat]:[add part three:hash]

---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartThreeHashTest.java
@@ -0,0 +1,18 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/7/21
+ */
+@SpringBootTest
+public class PartThreeHashTest {
+
+    @Autowired
+    private PartThreeHash partThreeHash;
+
+
+}
