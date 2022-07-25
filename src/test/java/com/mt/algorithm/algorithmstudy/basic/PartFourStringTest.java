b9ac1e034e888c80550e39b4b99614694eb9a55e
KluasMM <matian0720@foxmail.com>
2022-07-25 21:25:53 +0800

    [feat]:[init part four string]

---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartFourStringTest.java
@@ -0,0 +1,18 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/7/25
+ */
+@SpringBootTest
+public class PartFourStringTest {
+
+    @Autowired
+    private PartFourString service;
+
+
+}
