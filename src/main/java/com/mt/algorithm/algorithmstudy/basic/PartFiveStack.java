b5b44aef585832454b7c351e16aa435b533f5fc8
KluasMM <matian0720@foxmail.com>
2022-07-31 11:31:32 +0800

    [feat]:[add part five: stack]

---
+++ src/main/java/com/mt/algorithm/algorithmstudy/basic/PartFiveStack.java
@@ -0,0 +1,14 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/7/31
+ */
+@Service
+public class PartFiveStack {
+
+
+}
---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartFiveStackTest.java
@@ -0,0 +1,18 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/7/31
+ */
+@SpringBootTest
+public class PartFiveStackTest {
+
+    @Autowired
+    private PartFiveStack service;
+
+
+}
