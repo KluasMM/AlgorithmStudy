9f446b60556f5308b01445e33dbbab46b96688c9
KluasMM <matian0720@foxmail.com>
2022-08-01 22:02:22 +0800

    [feat]:[add part six: binary tree]

---
+++ src/main/java/com/mt/algorithm/algorithmstudy/basic/PartSixBinaryTree.java
@@ -0,0 +1,13 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/8/1
+ */
+@Service
+public class PartSixBinaryTree {
+
+}
---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartSixBinaryTreeTest.java
@@ -0,0 +1,17 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/8/1
+ */
+@SpringBootTest
+public class PartSixBinaryTreeTest {
+
+    @Autowired
+    private PartSixBinaryTree service;
+
+}
