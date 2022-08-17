4fb5414bd292007dc4226ab19fdae6507f758d2f
KluasMM <matian0720@foxmail.com>
2022-08-17 21:53:17 +0800

    [feat]:[add part seven: backtrack]

---
+++ src/main/java/com/mt/algorithm/algorithmstudy/basic/PartSevenBacktrack.java
@@ -0,0 +1,14 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/8/17
+ */
+@Service
+public class PartSevenBacktrack {
+
+
+}
---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartSevenBacktrackTest.java
@@ -0,0 +1,17 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/8/17
+ */
+@SpringBootTest
+public class PartSevenBacktrackTest {
+
+    @Autowired
+    private PartSevenBacktrack service;
+
+}
