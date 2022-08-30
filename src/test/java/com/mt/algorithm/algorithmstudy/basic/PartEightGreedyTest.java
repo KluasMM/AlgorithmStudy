4f4489cbe45003ac32a1a22d882cded262aab5da
KluasMM <matian0720@foxmail.com>
2022-08-30 22:37:55 +0800

    [feat]:[add part eight: greedy]

---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartEightGreedyTest.java
@@ -0,0 +1,17 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/8/30
+ */
+@SpringBootTest
+public class PartEightGreedyTest {
+
+    @Autowired
+    private PartEightGreedy service;
+
+}
