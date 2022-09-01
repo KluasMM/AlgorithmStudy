e6479a227b569f184459a376c79efeb0a5317d9b
KluasMM <matian0720@foxmail.com>
2022-09-01 22:27:47 +0800

    [feat]:[add part nine: dp]

---
+++ src/test/java/com/mt/algorithm/algorithmstudy/basic/PartNineDpTest.java
@@ -0,0 +1,17 @@
+package com.mt.algorithm.algorithmstudy.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author T
+ * @Date 2022/9/1
+ */
+@SpringBootTest
+public class PartNineDpTest {
+
+    @Autowired
+    private PartNineDp service;
+
+}
