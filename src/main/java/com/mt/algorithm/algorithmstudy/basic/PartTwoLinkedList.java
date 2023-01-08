3a7f3e78faab72e630cd28a6d95a5568dbd72454
KluasMM <matian0720@foxmail.com>
2023-01-08 22:46:29 +0800

    [feat]:[add 5]

--- src/main/java/com/mt/algorithm/algorithmstudy/basic/PartTwoLinkedList.java
+++ src/main/java/com/mt/algorithm/algorithmstudy/basic/PartTwoLinkedList.java
@@ -26,6 +26,11 @@ public class PartTwoLinkedList {
         ListNode(int val) {
             this.val = val;
         }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
     /**
